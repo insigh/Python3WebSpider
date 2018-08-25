3ce6b04bef04bdeacc6e05534cb0576a8e25f850
insigh <zhangchaojie@ruc.edu.cn>
2018-08-25 22:40:02 +0800

    25-08-2018

---
+++ windows/test.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pymysql
+import pymongo
+from flask import Flask
+import tornado
+import lxml
+import scrapy
+
+
+soup = BeautifulSoup('<p>Hello<p>', 'lxml')
+print(soup.p.string)
+
+
+# browser = webdriver.PhantomJS()
+# browser.get("https://www.baidu.com")
+# print(browser.current_url)
